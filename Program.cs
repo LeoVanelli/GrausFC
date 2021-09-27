019020fd603d5a8359557a96aecdc49389e0eef3
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:40:48 -0300

    Adiciona atividade GrsusFC

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Converta uma temperatura digitada pelo usuário em °F para °C.
+
+            Console.Write("Digite o valor da temperatura em ºF: ");
+            int F = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nResposta: {F}ºF equivalem à {Math.Round(((F-32)/1.8),2)}ºC");
+        }
+    }
+}
